5818f90e3df062009e17a297c89e2c1b5d8e9977
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-05-07 18:19:56 +0300

    arrays

---
+++ codeacad6_arrays.js
@@ -0,0 +1,10 @@
+var languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"];
+
+console.log(languages[2]);
+console.log(languages.length);
+
+for (i =0; i<languages.length; i++){
+    console.log(languages[i]);
+}
+
+var newArray = [["A", "S"], [21, 300]];
