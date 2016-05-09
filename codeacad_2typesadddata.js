da7af6ea281dcdca03de781d8a8f204c488556d0
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-05-09 23:20:47 +0300

    how to add data in two ways

---
+++ codeacad_2typesadddata.js
@@ -0,0 +1,17 @@
+var friends = {};
+friends.bill = {};
+friends.steve = {};
+
+friends.bill.firstName = "Bill";
+friends.bill.lastName = "Gates";
+friends.bill.number = "773-773-7733";
+friends.bill.address = ['One Microsoft Way', 'Redmond', 'WA', '98052'];
+
+friends.steve = {
+    firstName: "Steve",
+    lastName: "Jobs",
+    number: "206-206-2006",
+    address: ['Two Apple Ave', 'Chicago', 'IL', '60052']
+}
+
+console.log(friends);
