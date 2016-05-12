e74905ba35290a8566d5522a8db5f27fdf95ec6d
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-05-13 00:04:48 +0300

    case 2

--- codeacad_case.js
+++ codeacad_case.js
@@ -2,6 +2,10 @@ var getReview = function (movie) {
     switch(movie) {
     case "Toy Story 2":
         return  "Great story. Mean prospector."
+    case "Finding Nemo":
+        return "Cool animation, and funny turtles."
+    
+    
     }
 
 };
