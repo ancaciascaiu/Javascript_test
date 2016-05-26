45b86a36ea33d8482aa17a99b1b67b745ffeba03
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-05-26 18:42:56 -0500

    add more data

--- codeacad_function.js
+++ codeacad_function.js
@@ -7,5 +7,13 @@ friends.bill.lastName = "Gates";
 friends.bill.number = "773-773-7733";
 friends.bill.address = ['One Microsoft Way', 'Redmond', 'WA', '98052'];
 
+friends.steve = {
+    firstName: "Steve",
+    lastName: "Jobs",
+    number: "206-206-2006",
+    address: ['Two Apple Ave', 'Chicago', 'IL', '60052']
+}
+
+
 
 list(friends);
