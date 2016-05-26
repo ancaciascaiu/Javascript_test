04093d213ed29c2a450775fe2df4d6242e87a7aa
Anca Fodor Ciascaiu <anca_ciascaiu@yahoo.com>
2016-05-26 18:42:06 -0500

    make obj, add info

---
+++ codeacad_function.js
@@ -0,0 +1,11 @@
+var friends = {};
+friends.bill = {};
+friends.steve = {};
+
+friends.bill.firstName = "Bill";
+friends.bill.lastName = "Gates";
+friends.bill.number = "773-773-7733";
+friends.bill.address = ['One Microsoft Way', 'Redmond', 'WA', '98052'];
+
+
+list(friends);
